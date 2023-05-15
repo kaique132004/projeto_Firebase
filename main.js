a2b6ae2c4f7d2d83b33a0c7978f950db4f4297d2
Kaique Barbosa <kaique.235.fly@gmail.com>
2023-05-15 19:53:08 -0300

    three commit

--- main.js
+++ main.js
@@ -3,56 +3,62 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Obtenha referências para os elementos HTML
 
-    var emailInput = document.getElementById("email");
-    var passwordInput = document.getElementById("senha");
-    var loginButton = document.getElementById("login-button");
-    var logoutButton = document.getElementById("logout-button");
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log("Usuario Conectado!!");
+        } else {
+            var emailInput = document.getElementById("email");
+            var passwordInput = document.getElementById("senha");
+            var loginButton = document.getElementById("login-button");
+            var logoutButton = document.getElementById("logout-button");
 
-    var loginSenha = document.getElementById("login_senha");
+            var loginSenha = document.getElementById("login_senha");
 
-    // Adicione um evento de clique para o botão de login
-    loginButton.addEventListener("click", function () {
-        var provider = new firebase.auth.GoogleAuthProvider();
+            // Adicione um evento de clique para o botão de login
+            loginButton.addEventListener("click", function () {
+                var provider = new firebase.auth.GoogleAuthProvider();
 
-        firebase.auth().signInWithPopup(provider)
-            .then(function (result) {
-                // O usuário fez login com sucesso
-                var user = result.user;
-                console.log("Usuário logado:", user.displayName);
-            })
-            .catch(function (error) {
-                // Ocorreu um erro durante o login
-                console.error("Erro de login:", error);
+                firebase.auth().signInWithPopup(provider)
+                    .then(function (result) {
+                        // O usuário fez login com sucesso
+                        var user = result.user;
+                        console.log("Usuário logado:", user.displayName);
+                    })
+                    .catch(function (error) {
+                        // Ocorreu um erro durante o login
+                        console.error("Erro de login:", error);
+                    });
             });
-    });
 
-    // Adicione um evento de clique para o botão de logout
-    logoutButton.addEventListener("click", function () {
-        firebase.auth().signOut()
-            .then(function () {
-                // O usuário fez logout com sucesso
-                console.log("Usuário deslogado");
-            })
-            .catch(function (error) {
-                // Ocorreu um erro durante o logout
-                console.error("Erro de logout:", error);
+            // Adicione um evento de clique para o botão de logout
+            logoutButton.addEventListener("click", function () {
+                firebase.auth().signOut()
+                    .then(function () {
+                        // O usuário fez logout com sucesso
+                        console.log("Usuário deslogado");
+                    })
+                    .catch(function (error) {
+                        // Ocorreu um erro durante o logout
+                        console.error("Erro de logout:", error);
+                    });
             });
-    });
 
-    loginSenha.addEventListener("click", function () {
-        var email = emailInput.value;
-        var senha = passwordInput.value;
+            loginSenha.addEventListener("click", function () {
+                var email = emailInput.value;
+                var senha = passwordInput.value;
 
-        firebase.auth().signInWithEmailAndPassword(email, senha)
-        .then(function(result) {
-            // O usuário fez login com sucesso
-            var user = result.user;
-            console.log("Usuário logado:", user.displayName);
-        })
-        .catch(function(error) {
-            // Ocorreu um erro durante o login
-            console.error("Erro de login:", error);
-        });
-    });
+                firebase.auth().signInWithEmailAndPassword(email, senha)
+                    .then(function (result) {
+                        // O usuário fez login com sucesso
+                        var user = result.user;
+                        console.log("Usuário logado:", user.displayName);
+                    })
+                    .catch(function (error) {
+                        // Ocorreu um erro durante o login
+                        console.error("Erro de login:", error);
+                    });
+            });
+        }
+    })
 
 });
